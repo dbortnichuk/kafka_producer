874a3534a96c7fb073c5e3a08c5804ab02c7802d
unknown <dbort@DALMATIAN.softservecom.com>
2015-10-20 13:04:05 +0300

    Initial commit

---
+++ src/main/scala/com/cisco/mantl/kafka/KProdDriver.scala
@@ -0,0 +1,12 @@
+package com.cisco.mantl.kafka
+
+/**
+ * Created by dbort on 20.10.2015.
+ */
+object KProdDriver {
+
+  def main(args: Array[String]) {
+    println("Hello")
+  }
+
+}
---
+++ src/test/scala/com/cisco/mantl/kafka/KProdSuite.scala
@@ -0,0 +1,11 @@
+package com.cisco.mantl.kafka
+
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.FunSuite
+
+/**
+ * Created by dbort on 20.10.2015.
+ */
+class KProdSuite extends FunSuite with BeforeAndAfterAll {
+
+}
